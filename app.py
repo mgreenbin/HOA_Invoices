e7ed9b9b576286bc0230b97413c43cfc9cba14c8
Mark Greenberg <magreenberg@gmail.com>
2020-03-24 21:00:02 -0500

    Added try/catch, logging, and Path for generic path URL (macOS and Windows)

--- app.py
+++ app.py
@@ -1,104 +1,145 @@
+import logging
 import os
-
+import sys
 import openpyxl
 from openpyxl.styles import Font
-from openpyxl.drawing.image import Image
 from openpyxl.styles.alignment import Alignment
 
 cellsToValidate = ['A2', 'A8', 'G9', 'G10', 'A24', 'D24', 'H24', 'J24', 'H29', 'J29', 'J30']
-excelFolder = "c:/Users/mark/PycharmProjects/excel/"
 
-# create new folder for updated files
-updateFolder = excelFolder + "updates/"
-if not (os.path.exists(updateFolder)):
-    os.mkdir(updateFolder)
+excelFolder = "/Users/mark/PycharmProjects/excel/"
 
-def create_new_filename(fileName):
-    parts = fileName.split()    #split on the space delimiter (default)
-    parts[1] = "2020"   #2nd element of list contains the year, update it to the current year
-    newFileName = " ".join(parts)   #put it all back together again containg new year, delimited by spaces
-    return newFileName
+# excelFolder = "/Users/mark/Documents/Sterling Arbor Homeowners Documents/"
 
-def update_cell(cellToUpdate, newValue):
-    cell = ws[cellToUpdate]  # A2, C5, A8, etc....
-    cell.value = newValue  # string, but maybe date or numeric if necessary
-    # cell.alignment = Alignment(horizontal = "center", vertical = "center")
-    return cell
+def get_logger():
+    formatstr = "%(asctime)s %(levelname)-8s [%(funcName)s:%(lineno)d] %(message)s"
+    logging.basicConfig(
+        format=formatstr,
+        level=logging.DEBUG,
+        filename="HOA_Invoices.log")
+    logger = logging.getLogger("App.log")
+    return logger
 
 
-with os.scandir(excelFolder) as it:  # it = iterator returned by os.scandir(path)
-    for dirEntry in it:
-        if dirEntry.name.endswith(".xlsx"):
-            # print("Entry name: " + entry.name)
-            print("Entry path: " + dirEntry.path)
+def main():
+    try:
+        logger = get_logger()
+        logger.info("Beginning main()....")
 
-            # process spreadsheets, one for each dir entry
-            wb = openpyxl.load_workbook(dirEntry.path)
+        # create new folder for updated files
+        updateFolder = excelFolder + "updates/"
+        if not (os.path.exists(updateFolder)):
+            os.mkdir(updateFolder)
+            logger.info(f"{updateFolder} created for updates.")
 
-            # get reference to worksheet
-            ws = wb.active
+        def create_new_filename(fileName):
+            parts = fileName.split()  # split on the space delimiter (default)
+            parts[1] = "2020"  # 2nd element of list contains the year, update it to the current year
+            newFileName = " ".join(parts)  # put it all back together again containg new year, delimited by spaces
+            return newFileName
 
-            # set picture - Install Pillow?
-           #img = Image("image004.png")
-            #ws.add_image(img, "A5")
+        def update_cell(cellToUpdate, newValue):
+            cell = ws[cellToUpdate]  # A2, C5, A8, etc....
+            cell.value = newValue  # string, but maybe date or numeric if necessary
+            return cell
 
-            # invoice date
-            cell = update_cell("A2", "2020 Invoice")
-            cell.alignment = Alignment(horizontal="center", vertical="center")
+        with os.scandir(excelFolder) as it:  # it = iterator returned
+            for dirEntry in it:
+                if dirEntry.name.endswith(".xlsx"):
+                    # print("Entry name: " + dirEntry.name)
+                    # print("Entry path: " + dirEntry.path)
 
-            # officer contact info
-            cell = update_cell("A8", "Mark Greenberg - President\n205-505-9216")
-            cell.alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)
-            cell.font = Font(name='Arial', size=12, b=True)
+                    # process spreadsheets, one for each dir entry
+                    wb = openpyxl.load_workbook(dirEntry.path)
+                    with wb as wrkbk:
+                        logger.info(f"Loaded {dirEntry.path}....")
+                        # get reference to worksheet
+                        ws = wb.active
+                        # ws.protection.disable()
+                        # wb.save()
 
-            # title
-            cell = update_cell("C5", "Sterling Arbor\nNeighborhood Association, Inc.")
-            cell.alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)
-            cell.font = Font(name='Arial', size=12, b=True)
+                        # set picture in A5 - Remember to install Pillow; pip install Pillow
+                        # img = Image("image004.png")
+                        # ws.add_image(img, "A5")
 
-            # billing date
-            cell = update_cell("G9", "Billing Date: 09/30/2020")
-            cell.alignment = Alignment(horizontal="left", vertical="center")
+                        # invoice date
+                        cell = update_cell("A2", "2020 Invoice")
+                        cell.alignment = Alignment(horizontal="center", vertical="center")
 
-            # due date
-            cell = update_cell("G10", "Due Date....: 10/15/2020")
-            cell.alignment = Alignment(horizontal="left", vertical="center")
+                        # officer contact info
+                        cell = update_cell("A8", "Mark Greenberg - President\n205-505-9216")
+                        cell.alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)
+                        cell.font = Font(name='Arial', size=12, b=True)
 
-            # activity date
-            cell = update_cell("A24", "1/01/2020")
-            cell.alignment = Alignment(horizontal="center", vertical="center")
+                        # title
+                        cell = update_cell("C5", "Sterling Arbor\nNeighborhood Association, Inc.")
+                        cell.alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)
+                        cell.font = Font(name='Arial', size=12, b=True)
 
-            # detail description
-            desc = "Homeowner Dues for Year 2020"
-            cell = update_cell("D24", desc)
-            cell.alignment = Alignment(horizontal="center", vertical="center")
+                        # billing date
+                        cell = update_cell("G9", "Billing Date: 01/01/2020")
+                        cell.alignment = Alignment(horizontal="left", vertical="center")
 
-            # amount due
-            update_cell("H24", "$100")
+                        # due date
+                        cell = update_cell("G10", "Due Date....: 04/15/2020")
+                        cell.alignment = Alignment(horizontal="left", vertical="center")
 
-            # payment
-            update_cell("J24", "$0.00")
+                        # activity date
+                        cell = update_cell("A24", "1/01/2020")
+                        cell.alignment = Alignment(horizontal="center", vertical="center")
 
-            # total amount due
-            update_cell("H29", "$100.00")
+                        # detail description
+                        desc = "Homeowner Dues for Year 2020"
+                        cell = update_cell("D24", desc)
+                        cell.alignment = Alignment(horizontal="center", vertical="center")
 
-            # total payments
-            update_cell("J29", "$0.00")
+                        # amount due
+                        update_cell("H24", "$100")
 
-            # amount due
-            update_cell("J30", "$100.00")
+                        # payment
+                        update_cell("J24", "$0.00")
 
-            # mail to
-            mailTo = "Mark Greenberg, 6983 Sterling Ln, Trussville, AL  35173"
-            cell = update_cell("C34", mailTo)
-            cell.alignment = Alignment(horizontal="left", vertical="center")
+                        # total amount due
+                        update_cell("H29", "$100.00")
 
+                        # total payments
+                        update_cell("J29", "$0.00")
 
-            # save updated files to /updates folder, leaving original file as is.
-            newFileName = create_new_filename(dirEntry.name)
+                        # amount due
+                        update_cell("J30", "$100.00")
 
-            saveFileName = updateFolder + newFileName  # concat new file name with new folder name
-            print("dirEntry.Name: " + dirEntry.name)
-            print("saveAsName: " + saveFileName)
-            wb.save(saveFileName)
-            wb.close()
+                        # mail to
+                        mailTo = "Mark Greenberg, 6983 Sterling Ln, Trussville, AL  35173"
+                        cell = update_cell("C34", mailTo)
+                        cell.alignment = Alignment(horizontal="left", vertical="center")
+
+                        # save updated files to /updates folder, leaving original file as is.
+                        newFileName = create_new_filename(dirEntry.name)
+
+                        saveFileName = updateFolder + newFileName  # concat new file name with new folder name
+                        # print("dirEntry.Name: " + dirEntry.name)
+                        # print("saveAsName: " + saveFileName)
+
+                        # set print area
+                        ws.print_area = "A1:K49"
+
+                        wb.save(saveFileName)
+                        wb.close()
+
+                    logger.info(f"Updated {saveFileName} closed!")
+
+    except (RuntimeError, TypeError, NameError) as err:
+        logger.critical("Unexpected Error Occurred", sys.exc.info()[0])
+        logger.critical(f"Unexpected Error Occurred, error info {err}")
+
+    except:
+        logger.critical("Unexpected Error Occurred", sys.exc.info()[0])
+
+    finally:
+        wb.close()
+        logger.info(f"Updated {saveFileName} closed in finally!")
+
+    if __name__ == '__main__':
+        logger = get_logger()
+        main()
+        logger.info("End main()....")
--- mg_logger.py
+++ mg_logger.py
@@ -1,11 +1,10 @@
 import logging
 
-
 formatstr = "%(asctime)s %(levelname)-8s [%(funcName)s:%(lineno)d] %(message)s"
 logging.basicConfig(
     format=formatstr,
     level=logging.DEBUG,
-    filename="AppLog.txt")
+    filename="HOA_Invoices.log")
 
 logger = logging.getLogger("App.log")
 logger.warning("This is a warning: Get ready, it's about to BLOW")
